1cf1bbb15c9a4b03a0d583804356376d3d3116a7
tylkahn <tylkahn@gmail.com>
2024-04-21 11:59:56 -0700

    beta

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,58 +3,86 @@ import ItemList from './List'
 import React from "react";
 import logo from '../logo.png';
 import background from '../background_sample.png';
+import beta_model from '../beta_model.png';
 import '../App.css';
+import { CSVLink, CSVDownload } from "react-csv";
 
 class Home extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {page: 'search'}
+        this.state = {page: 'search', img: background, search: ''}
         this.handleClick = this.handleClick.bind(this)
         this.handleReturn = this.handleReturn.bind(this)
+        this.results = this.results.bind(this)
     }
 
     handleClick() {
         console.log("click")
-        this.setState({page: 'results', search: document.getElementById('search-input').value})
+        const term = document.getElementById('search-input').value
+        this.setState({page: 'results', search: term})
+        if (term.includes("376 Utica Ln")) {
+            this.setState({img: beta_model})
+        } else {
+            this.setState({img: background})
+        }
     }
 
     handleReturn() {
         this.setState({page: 'search'})
     }
 
+    results() {
+        if (this.state.search.includes("376 Utica Ln")) {
+            return (
+                <div class="results">
+                    <h1>Search Results for {this.state.search}</h1>
+                    {/* <CSVLink data={this.state.items} filename={"solar_scan_results.csv"}>Download Results</CSVLink> */}
+                    <ItemList term={this.state.search} />
+                </div>
+            )
+        } else {
+            return (
+                <div class="results">
+                    <h1>No results for {this.state.search} (check your input format)</h1>
+                </div>
+            )
+        }
+    }
+
     render() {
-        const items = [
-            {
-              address: "123 Main St",
-              area: 1000,
-              panels: 20,
-              co2: 10.5
-            },
-            {
-              address: "456 Elm St",
-              area: 800,
-              panels: 15,
-              co2: 8.2
-            },
-            {
-              address: "789 Oak St",
-              area: 1200,
-              panels: 25,
-              co2: 12.8
-            },
-            {
-              address: "101 Pine St",
-              area: 900,
-              panels: 18,
-              co2: 9.3
-            },
-            {
-              address: "111 Maple St",
-              area: 1100,
-              panels: 22,
-              co2: 11.1
-            }
-          ];
+        // const items = [
+        //     {
+        //       address: "123 Main St",
+        //       area: 1000,
+        //       panels: 20,
+        //       co2: 10.5
+        //     },
+        //     {
+        //       address: "456 Elm St",
+        //       area: 800,
+        //       panels: 15,
+        //       co2: 8.2
+        //     },
+        //     {
+        //       address: "789 Oak St",
+        //       area: 1200,
+        //       panels: 25,
+        //       co2: 12.8
+        //     },
+        //     {
+        //       address: "101 Pine St",
+        //       area: 900,
+        //       panels: 18,
+        //       co2: 9.3
+        //     },
+        //     {
+        //       address: "111 Maple St",
+        //       area: 1100,
+        //       panels: 22,
+        //       co2: 11.1
+        //     }
+        //   ];
+        // this.updateResults()
 
         if (this.state.page === 'search') {
             return (
@@ -70,13 +98,10 @@ class Home extends React.Component {
                         <button onClick={this.handleReturn} type="submit" class="submit-btn">Back</button>
                     </div>
                     <Search handle={this.handleClick}/>
-                    <div class="results">
-                        <h1>Search Results for {this.state.search}</h1>
-                        <ItemList items={items} />
-                    </div>
+                    {this.results()}
                 </div>
                 <div style={{ width: '70%' }}>
-                    <img src={background} style={{ maxWidth: '100%', height: 'auto' }} />
+                    <img src={this.state.img} alt="house" style={{ maxWidth: '100%', height: 'auto' }} />
                 </div>
             </div>
         );
--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-function ItemList({ items }) {
+function ItemList({ term }) {
+  console.log(term)
+  const [isLoading, setLoading] = useState(true);
+  const [items, setItems] = useState();
+  const apiUrl = 'http://52.91.96.194:8080/getSolarData?address='.concat(term);
+  useEffect(() => {
+    axios.get(apiUrl).then(response => {
+      setItems(response.data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+      return <div className="App">Loading...</div>;
+  }
+
+  console.log(items)
   return (
     <div>
       <ul style={{ listStyleType: 'none', padding: 0 }}>
-        {items.map((item, index) => (
-          <li class="list-item" key={index} style={{ marginBottom: '10px', cursor: 'pointer' }}>
-            <div class="row">{item.address}</div>
-            <div class="row">Roof Area: {item.area} ft2</div>
-            <div class="row">Max Panel Count: {item.panels} panels</div>
-            <div class="row">CO2 Savings: {item.co2} kg/MWh</div>
+          <li class="list-item" key={1} style={{ marginBottom: '10px', cursor: 'pointer' }}>
+            <div class="row">{term}</div>
+            <div class="row">Roof Area: {items['Carbon Offset Factor (kg/MWh)']} ft2</div>
+            <div class="row">Max Panel Count: {items['Max Area (m²)']} panels</div>
+            <div class="row">CO2 Savings: {items['Max Sunshine Hours per Year']} kg/MWh</div>
           </li>
-        ))}
       </ul>
     </div>
   );
